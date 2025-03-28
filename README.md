4f57b7fe14edad99e8c05069cac11b6a48adf15d
Jeffrey <38684@ma-web.nl>
2025-03-28 12:58:28 +0100

    tutorial 3

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 # Tutorial
 
 ## Tutorial 0
-![Cube getting launched upwards and falling down](Prototype.gif)
+![Cube getting launched upwards and falling down](Prototype.gif)  
 [Link to script](https://github.com/Shirge/Tutorial/blob/main/Tutorial/Assets/Scripts/LaunchCube.cs)
 
 ## Tutorial 1
-![Animation of default mixamo character dying](Animation.gif)
+![Animation of default mixamo character dying](Animation.gif)  
 [Link to script](https://github.com/Shirge/Tutorial/blob/main/Tutorial/Assets/Scripts/CharacterAnimation.cs)
 
 ## Tutorial 2
-![Animation of a character driving around](Moving.gif)
-Links to script  
+![Animation of a character driving around](Moving.gif)  
+Links to scripts  
 [Animation](https://github.com/Shirge/Tutorial/blob/main/Tutorial/Assets/Scripts/WalkingAnimation.cs)  
 [Basic Movement](https://github.com/Shirge/Tutorial/blob/main/Tutorial/Assets/Scripts/MoveBasic.cs)  
 [Basic Movement (No turning)](https://github.com/Shirge/Tutorial/blob/main/Tutorial/Assets/Scripts/MoveNoTurn.cs)
+
+## Tutorial 3
+![Cube moving and jumping around](Jumping.gif)
+[Link to the script](https://github.com/Shirge/Tutorial/blob/main/Tutorial/Assets/Scripts/Movement.cs)
---
+++ Tutorial/Assets/Scripts/Movement.cs
@@ -0,0 +1,60 @@
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float rotationSpeed;
+
+    private Rigidbody rigidbody;
+    private Vector3 moveDirection;
+    private bool onFloor;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        onFloor = false;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "floor")
+        {
+            onFloor = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        onFloor = false;
+    }
+
+    private void FixedUpdate()
+    {
+        // Gravity * 8
+        if (!onFloor)
+            rigidbody.AddForce(Physics.gravity * 7);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && onFloor)
+        {
+            rigidbody.AddForce(0, 50f, 0, ForceMode.Impulse);
+            onFloor = false;
+        }
+
+        transform.Rotate(new Vector3(0, Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime, 0));
+
+        if (Input.GetAxisRaw("Vertical") == 0)
+        {
+            rigidbody.linearVelocity = new Vector3(0, rigidbody.linearVelocity.y, 0);
+            return;
+        }
+        else
+        {
+            moveDirection = Input.GetAxisRaw("Vertical") * transform.forward * speed * Time.deltaTime;
+            rigidbody.AddForce(moveDirection.normalized * speed, ForceMode.Force);
+        }
+    }
+}
